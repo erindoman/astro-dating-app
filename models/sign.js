5db891f7040e6ee33e579b0988c94c14e3999552
erindoman <erin.doman@gmail.com>
2020-12-18 09:39:40 -0600

    finish delete function, MVP

--- models/sign.js
+++ models/sign.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-  commenter: String,
-  rating: {type: Number, min: 1, max: 5},
-  content: String
-})
+// const commentSchema = new Schema({
+//   commenter: String,
+//   rating: {type: Number, min: 1, max: 5},
+//   content: String
+// })
 
 const signSchema = new Schema ({
   sign: {type: String, enum: ['Scorpio', 'Aquarius', 'Cancer', 'Pisces', 'Aries', 'Sagittarius', 'Capricorn', 'Libra', 'Virgo', 'Gemini', 'Leo', 'Taurus']},
   commenter: String,
   rating: {type: Number, min: 1, max: 5},
-  content: String},{
+  content: String
+},{
   timestamps: true
 })
 
